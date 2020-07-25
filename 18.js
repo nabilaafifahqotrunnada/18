9425ad862cf9083e17251938bcf1999962f97bbe
nabilaafifahqotrunnada <nabila_afifah_28rpl@student.smktelkom-mlg.sch.id>
2020-07-26 00:00:34 +0700

    Second Commit

---
+++ 18.js
@@ -0,0 +1,18 @@
+//Do.. While loop
+
+//membuat array gadget
+let gadget =
+[
+    "Xiaomi", "Samsung", "Motorola", "Sony Ericson"
+]
+
+//kondisi awal
+let i = 0
+
+//mengeksekusi sekali sebelum pengecekan
+do{
+    console.log(gadget[i]);
+    i++
+}
+//pengecekan
+while(gadget[i])
